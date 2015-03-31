3d81c74dad434f8321e28b1126d50b0c76517a22
Chris Lyons <chris.lyons@nutonian.com>
2015-03-31 11:58:12 +0200

    Grunt build part.1

--- test/Test.spec.js
+++ test/Test.spec.js
@@ -1,3 +1,9 @@
-var bar = 'bar';
-
-console.log(bar);
+define(['Nugget'], function(Nugget) {
+    describe('Modules', function () {
+        it('should have a bunch of modules', function() {
+            expect(Nugget.DataSeries).toBeDefined();
+            expect(Nugget.Chart).toBeDefined();
+            expect(Nugget.LineGraph).toBeDefined();
+        });
+    });
+});
