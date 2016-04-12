6f71582502c6daac9b1851fb62fd6eb37332dc8d
lshrinivas <lakshmikant.shrinivas@nutonian.com>
2016-04-12 15:50:13 -0400

    Review comments + tests for AreaGraph

--- test/models/NumericalDataSeries.spec.js
+++ test/models/NumericalDataSeries.spec.js
@@ -15,20 +15,28 @@ function (
             dataSeries = new Nugget.NumericalDataSeries();
         });
 
-        it('validate data', function() {
+        it('should validate data', function() {
             expect(function() { dataSeries.setData([1, 2, 3]); }).toThrow();
             expect(function() { dataSeries.setData([{x_value: '1', y_value: '2'}]); }).toThrow();
+
+            // positive cases
             expect(function() { dataSeries.setData([{x_value: 1, y_value: 2}]); }).not.toThrow();
             expect(function() { dataSeries.setData([
                 {x_value: 1, y_value: 2},
                 {y_value: 1, x_value: 2}
             ]); }).not.toThrow();
             expect(function() { dataSeries.setData([
+                {x_value: 1, x_min: 0, x_max: 3, y_value: 2, y_min: 1, y_max: 3},
+                {y_value: 1, y_min: 1, y_max: 3, x_value: 2, x_min: 0, x_max: 3}
+            ]); }).not.toThrow();
+
+            // all points should have same structure
+            expect(function() { dataSeries.setData([
                 {x_value: 1, y_value: 2},
-                {y_value: 1, x_value: 2, z: 3}
+                {y_value: 1, x_value: 2, y_min: 3}
             ]); }).toThrow();
             expect(function() { dataSeries.setData([
-                {y_value: 1, x_value: 2, z: 3},
+                {y_value: 1, x_value: 2, y_min: 3},
                 {x_value: 1, y_value: 2}
             ]); }).toThrow();
         });
